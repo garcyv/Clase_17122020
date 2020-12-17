7cc262897d14f2929ecc7a1f9c2f13da6d22ea26
garcy <garcyv@gmail.com>
2020-12-17 14:17:24 -0300

    Incorpora ViewBind Accesa boton y lo gira 180°

--- app/src/main/java/com/example/clase_17122020/MainActivity.java
+++ app/src/main/java/com/example/clase_17122020/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.clase_17122020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.clase_17122020.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding bind;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bind = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(bind.getRoot());
+        Button mButton = findViewById(R.id.bt_button);
+
+        bind.btButton.setRotationY(180);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Botón presionado",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        );
+
     }
+
+
 }
+
+
